622f38fd73a0f650052ba91dc9e775360af2366a
Maiken <maikenfjellanger@gmail.com>
2022-06-16 10:43:32 +0200

    Blad 1 og 2

--- src/components/plant.jsx
+++ src/components/plant.jsx
@@ -1,4 +1,6 @@
 import { ReactComponent as Stilk } from "../svg/plante/stilk.svg";
+import { ReactComponent as Blad1 } from "../svg/plante/blad1.svg";
+import { ReactComponent as Blad2 } from "../svg/plante/blad2.svg";
 import styled from "styled-components";
 import { useState } from "react";
 
@@ -10,6 +12,8 @@ const Plant = () => {
       <StyledSquare1 animating={animating} />
       <StyledSquare2 animating={animating} />
       <StyledStilk />
+      <StyledBlad1 animating={animating} />
+      <StyledBlad2 animating={animating} />
     </StyldContainer>
   );
 };
@@ -42,6 +46,36 @@ const StyledSquare2 = styled.div`
   ${({ animating }) =>
     animating ? "transition: transform 2.5s ease-out 1.5s;" : ""}
   ${({ animating }) => (animating ? "transform: translate(-22rem, 0);" : "")}
+`;
+
+const StyledBlad1 = styled(Blad1)`
+  position: absolute;
+  top: 33rem;
+  left: 18rem;
+  width: 0.1rem;
+  height: 0.1rem;
+  transform-origin: 0% 100%;
+
+  ${({ animating }) =>
+    animating
+      ? "transition: transform 1s cubic-bezier( 0.36, 0.44, 1, 0.25 ) 3s;"
+      : ""}
+  ${({ animating }) => (animating ? " transform: scale(16);" : "")}
+`;
+
+const StyledBlad2 = styled(Blad2)`
+  position: absolute;
+  top: 28rem;
+  left: 19.6rem;
+  width: 0.1rem;
+  height: 0.1rem;
+  transform-origin: 0% 40%;
+
+  ${({ animating }) =>
+    animating
+      ? "transition: transform 1s cubic-bezier( 0.59, -0.1, 0.05, 0.92 ) 5s;"
+      : ""}
+  ${({ animating }) => (animating ? " transform: scale(20);" : "")}
 `;
 
 const StyledStilk = styled(Stilk)`
